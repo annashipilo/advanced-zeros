41c390c78dcf4f1c2554a36a2a6019f4e726deac
annashipilo <a.a.shipilo@gmail.com>
2018-03-02 14:57:58 +0300

    advanced zeroes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+    var n = parseInt(number);
+    for (var i = 2; i <= base; i++) {
+        if (base % i == 0) {
+            var p = 0;
+            while (base % i == 0) {
+                p++;
+                base = parseInt(base / i);
+            }
+            var count = 0;
+            var num = number;
+            while (num / i > 0) {
+                count = parseInt(count + num / i);
+                num = parseInt(num / i);
+            }
+            n = Math.min(n, count / p);
+        }
+    }
+    return parseInt(n);
 }
