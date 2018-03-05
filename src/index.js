f42d606d700fecd7e339fbe37a573c0a407339f5
Anna <36773373+annashipilo@users.noreply.github.com>
2018-03-05 21:18:49 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 module.exports = function getZerosCount(number, base) {
     var n = parseInt(number);
+    var num = parseInt(number);
+    var x;
+    var count = 0;
     for (var i = 2; i <= base; i++) {
-        if (base % i == 0) {
-            var p = 0;
-            while (base % i == 0) {
-                p++;
+        if (base % i === 0) {
+            x = 0;
+            while (base % i === 0) {
+                x++;
                 base = parseInt(base / i);
             }
-            var count = 0;
-            var num = number;
             while (num / i > 0) {
                 count = parseInt(count + num / i);
                 num = parseInt(num / i);
